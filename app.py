a58471b48011b03ffab29d2122e28495bf1a3bad
andriibuimistr <andriibuimistr@gmail.com>
2021-01-25 03:43:46 +0200

    First commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+from resources.user import UserRegister
+from security import authenticate, identity
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'key'
+
+# The next line is not from the course. Is needed to make jwt_required work when there is no token in a request
+app.config['PROPAGATE_EXCEPTIONS'] = True
+api = Api(app)
+
+from db import db
+db.init_app(app)
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+jwt = JWT(app, authenticate, identity)
+
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(StoreList, '/stores')
+api.add_resource(UserRegister, '/register')
+
+
+if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
+    app.run(port=5000, debug=True)
